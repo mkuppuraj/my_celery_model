78dff937ae854e11b2e61e8afcc3cab87b77d212
Kuppuraj Muthu <mkupraj@gmail.com>
2015-12-10 13:05:54 +0530

    added relationships to the models.

--- phonebook/model/contact.py
+++ phonebook/model/contact.py
@@ -1,11 +1,14 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 from phonebook.model.base import Base
 
 class Contact(Base):
-    
+
     __tablename__ = 'contact'
 
     contact_idn = Column(Integer, primary_key=True)
     name = Column(String)
     nickname = Column(String)
+
+    phones = relationship("Phone", order_by="Phone.phone_id", backref="Contact")
--- phonebook/model/phone.py
+++ phonebook/model/phone.py
@@ -1,14 +1,17 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship, backref
 
 from phonebook.model.base import Base
 
 class Phone(Base):
-    
+
     __tablename__ = 'phone'
 
     phone_idn = Column(Integer, primary_key=True)
-    phone_number = Column(Number)
+    phone_number = Column(Integer)
     phone_type = Column(String)
-    is_active = Column(Number)
+    is_active = Column(Integer)
     country = Column(String)
     country_code = Column(String)
+
+    contact = relationship("Contact", backref=backref('addresses', order_by=id))
