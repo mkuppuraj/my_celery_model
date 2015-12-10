25d1f166304b86ac8f67ac06fac1599e30a436a7
Kuppuraj Muthu <mkupraj@gmail.com>
2015-12-10 13:05:19 +0530

    modified proper packaging tools.

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from setuptools import setup
-
+from setuptools import setup, find_packages
 setup(
         name='phonebook',
         author='Kuppuraj Muthu',
         version="0.1",
         author_email='mkupraj@gmail.com',
         url='https://github.com/mkuppuraj/phonebook',
-        packages = ['phonebook'],
+        packages = find_packages(),
         entry_points = {
             'console_scripts': [
                 'init_db = phonebook.scripts.db_utils:initialize'
